ff6134195fc1de0c633a8ef943ed2f78ca9878fd
Athul Acharya <athul@acharya.cc>
2023-05-24 09:29:19 -0700

    Initial Commit

---
+++ GoogleSpeechSwiftUIDemo/GoogleSpeechSwiftUIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoogleSpeechSwiftUIDemoApp.swift
+//  GoogleSpeechSwiftUIDemo
+//
+//  Created by Athul Acharya on 5/24/23.
+//
+
+import SwiftUI
+
+@main
+struct GoogleSpeechSwiftUIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
